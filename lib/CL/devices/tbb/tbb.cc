2b8c7eca4dd68f9f4d09c72549049c23549df3b9
Tobias Baumann <t.baumann@agdsn.de>
2020-07-11 13:50:03 +0200

    Move from blocked_range to blocked_range3d

--- lib/CL/devices/tbb/tbb.cc
+++ lib/CL/devices/tbb/tbb.cc
@@ -44,6 +44,7 @@ extern "C" {
 #include <unistd.h>
 #include <utlist.h>
 
+#include <tbb/blocked_range3d.h>
 #include <tbb/parallel_for.h>
 #include <tbb/partitioner.h>
 
@@ -577,44 +578,11 @@ pocl_tbb_write (void *data,
   memcpy ((char *)device_ptr + offset, host_ptr, size);
 }
 
-inline static void
-translate_wg_index_to_3d_index (unsigned index, size_t *index_3d, unsigned xy_slice, unsigned row_size)
-{
-  index_3d[2] = index / xy_slice;
-  index_3d[1] = (index % xy_slice) / row_size;
-  index_3d[0] = (index % xy_slice) % row_size;
-}
-
-class WorkGroupScheduler {
-  _cl_command_node *my_k;
-  void **my_arguments;
-  
-public:
-  void operator()( const tbb::blocked_range<size_t>& r ) const {
-    _cl_command_node *k = my_k;
-    uint8_t *arguments = reinterpret_cast<uint8_t*>(my_arguments);
-    
-    size_t gids[3];
-    unsigned slice_size = k->command.run.pc.num_groups[0] * k->command.run.pc.num_groups[1];
-    unsigned row_size = k->command.run.pc.num_groups[0];
-    
-    for( size_t i=r.begin(); i!=r.end(); ++i ) {
-      translate_wg_index_to_3d_index(i, gids, slice_size, row_size);
-      ((pocl_workgroup_func) k->command.run.wg) (arguments, (uint8_t *)&k->command.run.pc, gids[0], gids[1], gids[2]);
-    }
-  }
-
-  WorkGroupScheduler( _cl_command_node *k, void **arguments ) :
-      my_k(k), my_arguments(arguments)
-  {}
-};
-
 void
 pocl_tbb_run (void *data, _cl_command_node *cmd)
 {
   struct data *d;
   struct pocl_argument *al;
-  size_t x, y, z;
   unsigned i;
   cl_kernel kernel = cmd->command.run.kernel;
   pocl_kernel_metadata_t *meta = kernel->meta;
@@ -714,12 +682,20 @@ pocl_tbb_run (void *data, _cl_command_node *cmd)
   unsigned ftz = pocl_save_ftz ();
   pocl_set_ftz (kernel->program->flush_denorms);
 
-  size_t n = pc->num_groups[0] * pc->num_groups[1] * pc->num_groups[2];
 #if defined(POCL_TBB_PARTITIONER_AFFINITY)
   tbb::affinity_partitioner ap;
 #endif
-  tbb::parallel_for (tbb::blocked_range<size_t>(0, n)
-                    , WorkGroupScheduler(cmd, arguments)
+  tbb::parallel_for (tbb::blocked_range3d<size_t>(0, pc->num_groups[0], 0, pc->num_groups[1], 0, pc->num_groups[2])
+                    , [&]( const tbb::blocked_range3d<size_t> &r )
+                      {
+                        for( size_t x=r.pages().begin(); x!=r.pages().end(); x++) {
+                          for( size_t y=r.rows().begin(); y!=r.rows().end(); y++) {
+                            for( size_t z=r.cols().begin(); z!=r.cols().end(); z++) {
+                              ((pocl_workgroup_func) cmd->command.run.wg) ((uint8_t*)arguments, (uint8_t*)&cmd->command.run.pc, x, y, z);
+                            }
+                          }
+                        }
+                      }
 #if defined(POCL_TBB_PARTITIONER_AFFINITY)
                     , ap
 #elif defined(POCL_TBB_PARTITIONER_SIMPLE)
